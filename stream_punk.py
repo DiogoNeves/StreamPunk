0707228f7130b402fc28c8649c0a50ec795e2cc6
Diogo Neves <diogo.neves@gmail.com>
2024-06-19 19:53:27 +0100

    messed around a bit more

--- stream_punk.py
+++ stream_punk.py
@@ -1,19 +1,19 @@
 from pynput import keyboard
 
-def on_press(key):
+def on_press(key: (keyboard.Key | keyboard.KeyCode | None)) -> None:
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print("Struct: ", key.__dict__)
+        if key.vk == 76:
+            print(f'{key} pressed')
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        return
 
-def on_release(key):
-    print('{0} released'.format(
-        key))
+def on_release(key: (keyboard.Key | keyboard.KeyCode | None)) -> bool:
+    print(f'{key} released')
     if key == keyboard.Key.esc:
         # Stop listener
         return False
+    return True
 
 
 if __name__ == "__main__":
@@ -23,8 +23,30 @@ if __name__ == "__main__":
             on_press=on_press,
             on_release=on_release) as listener:
         listener.join()
+    print("Bye!")
 
     # listener = keyboard.Listener(
     #     on_press=on_press,
     #     on_release=on_release)
     # listener.start()
+
+
+
+
+
+
+""" Notes
+
+Suppressing doesn't work yet, likely because the app is untrusted.
+
+key.vk = 76 => enter
+
+``uinput_device_paths``
+            A list of device paths.
+
+            If this is specified, *pynput* will limit the number of devices
+            checked for the capabilities needed to those passed, otherwise all
+            system devices will be used. Passing this might be required if an
+            incorrect device is chosen.
+
+"""
